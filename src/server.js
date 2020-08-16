706dac1a84d5ffe9682831dd043ca90311087825
yossy11 <srnnds50aky1@yahoo.co.jp>
2020-08-16 12:43:50 +0900

    allow cors

--- src/server.js
+++ src/server.js
@@ -4,8 +4,13 @@ const PORT = 3000;
 
 const app = express();
 
-app.get("/", (req, res) => res.send("This server hosts static files"));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
+app.get("/", (req, res) => res.send("This server hosts static files"));
 app.use(express.static("public"));
 
 app.listen(PORT);
